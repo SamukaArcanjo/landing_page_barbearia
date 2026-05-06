30e4559d7427ceb5c2962e4093b4a38e119003b0
SamukaArcanjo <samuelarcanjo57@gmail.com>
2026-05-06 19:04:35 -0300

    Deixei a header fixa e adicionei o efeito dela escurecer ao scrolar a tela com JS. Deixei tudo mais responsivo e terminei a second section.

--- src/nav_bar.js
+++ src/nav_bar.js
@@ -45,3 +45,18 @@ const mobileNavBar = new MobileNavBar(
 )
 
 mobileNavBar.init()
+
+
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 50) {
+        header.classList.add("scrolled");
+    } 
+    
+    else {
+        header.classList.remove("scrolled");
+    }
+
+});
